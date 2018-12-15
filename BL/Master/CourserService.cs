7dfd459431bac5011d7667f60ceafb182d821637
Nazih <Nazih@LAPTOP-HKI081UT>
2018-12-15 19:23:13 +0530

    Course API

---
+++ BL/Master/CourserService.cs
@@ -0,0 +1,42 @@
+﻿using BO;
+using BO.Master;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Master
+{
+    public class CourserService : IService<Course>
+    {
+        DL.Master.CourseRepository repository = new DL.Master.CourseRepository();
+
+        public List<Course> ToList => throw new NotImplementedException();
+
+        public ApiResponse<Course> Add(Course entity)
+        {
+            return repository.Add(entity);
+        }
+
+        public void Delete(long id)
+        {
+             repository.Delete(id);
+        }
+
+        public Course GetById(long Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ListQueryResult<Course> GetByQuery(ListQuery<Course> query)
+        {
+            return repository.GetByQuery(query);
+        }
+
+        public ApiResponse<Course> Update(Course entity)
+        {
+            return repository.Update(entity);
+        }
+    }
+}
---
+++ BO/QueryParameter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BO
+{
+    public class QueryParameter
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public DateRange Range { get; set; }
+        public CompareOperator Operator { get; set; }
+
+        public string MaxValue { get; set; }
+    }
+}
